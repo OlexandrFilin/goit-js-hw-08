a876fce5a75a444a1708bfa50b60df15fe0003b5
OleksandrFilin <acfiln@ukr.net>
2023-08-09 22:56:32 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,46 @@
+import Player from '@vimeo/player';
+throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
 
+const player = new Player(iframe, {
+  id: 19231868,
+  width: 1640,
+});
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+player.on('timeupdate', throttle(onTime, 1000));
+
+function onTime() {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+const videoPlCurTime = localStorage.getItem('videoplayer-current-time');
+if (videoPlCurTime) {
+  console.log(videoPlCurTime);
+  player
+    .setCurrentTime(Number(videoPlCurTime))
+    .then(function (seconds) {
+      //seconds = Number(videoPlCurTime);
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
